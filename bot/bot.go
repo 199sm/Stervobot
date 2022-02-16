41f6ab862f15ebe3b05ac9e00206996f3600289c
Oleg Danilov <oleoleolezha@gmail.com>
2022-02-16 23:52:44 +0300

    Added parsing song info

--- bot/bot.go
+++ bot/bot.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"github.com/antchfx/htmlquery"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	tele "gopkg.in/telebot.v3"
@@ -22,11 +24,35 @@ func main() {
 
 	b.Handle(tele.OnText, func(c tele.Context) error {
 		var (
-			//user = c.Sender()
-			text = c.Text()
+			text   = c.Text()
+			prefix = "https://open.spotify.com/track/"
 		)
 
-		return c.Send(text)
+		if strings.HasPrefix(text, prefix) {
+			doc, err := htmlquery.LoadURL(text)
+			if err != nil {
+				log.Print(err)
+			}
+
+			metaList, err := htmlquery.QueryAll(doc, "/html/head/meta")
+			if err != nil {
+				log.Print(err)
+			}
+
+			var title, info string
+			for _, meta := range metaList {
+				if meta.Attr[0].Val == "og:title" {
+					title = meta.Attr[1].Val
+					continue
+				}
+				if meta.Attr[0].Val == "og:description" {
+					info = meta.Attr[1].Val
+				}
+			}
+			return c.Send(title + " " + info)
+		}
+
+		return nil
 	})
 
 	b.Start()
--- bot/go.mod
+++ bot/go.mod
@@ -2,4 +2,14 @@ module github.com/199sm/Stervobot/bot
 
 go 1.17
 
-require gopkg.in/telebot.v3 v3.0.0
+require (
+	github.com/antchfx/htmlquery v1.2.4
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd
+	gopkg.in/telebot.v3 v3.0.0
+)
+
+require (
+	github.com/antchfx/xpath v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
